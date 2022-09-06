3b71b907d809531005aa9cd61e03fef7c3c23635
D'Artagnan Lamarca <37163261+skaiwolker@users.noreply.github.com>
2022-09-05 21:49:45 -0300

    First module .1
    OOP with CSharp

---
+++ bytebank_ADM/bytebank_ADM/Employees/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bytebank_ADM.Employers
+{
+    public class Employee
+    {
+        public string Name{ get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Balance { get; set; }
+
+
+        public double getBonus()
+        {
+            return Balance * 0.1;
+        }
+    }
+}
---
+++ bytebank_ADM/bytebank_ADM/Program.cs
@@ -0,0 +1,21 @@
+﻿using bytebank_ADM.Employers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bytebank_ADM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee jhon = new Employee();
+            jhon.Name = "Jhon";
+            jhon.Cpf = "123456789";
+            jhon.Balance = 2000;
+
+            Console.WriteLine("Bonification: " + jhon.getBonus());
+            Console.ReadLine();
+        }
+    }
+}
